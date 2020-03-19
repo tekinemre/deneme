17c48d2a8aa2dafa03e4aa00a84429b3a5b733ab
tekinemre <32355236+tekinemre@users.noreply.github.com>
2020-03-19 18:40:57 +0300

    Update reviewnb_trial.py

--- reviewnb_trial.py
+++ reviewnb_trial.py
@@ -8,7 +8,7 @@ Created on Thu Mar 19 11:55:33 2020
 #%%
 import pandas as pd
 
-df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]}, index=['a', 'a', 'b'])
+df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 111113]}, index=['a', 'a', 'b'])
 
 print(df.head())
 
