7e74f384b130b1d68d15f6078692172735a2ed8e
tekinemre <32355236+tekinemre@users.noreply.github.com>
2020-03-19 18:45:36 +0300

    ufak düzenlemeler
    ufak düzenlemeler

--- reviewnb_trial.py
+++ reviewnb_trial.py
@@ -8,15 +8,15 @@ Created on Thu Mar 19 11:55:33 2020
 #%%
 import pandas as pd
 
-df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 1111132323]}, index=['a', 'a', 'b'])
+df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2222, 1111132323]}, index=['a', 'a', 'b'])
 
 print(df.head())
 
-df2 = pd.DataFrame({'x': ['abc', None, 'def'],
+df_ = pd.DataFrame({'x': ['abc', None, 'defss'],
                    'y': [1, 2, np.nan],
                    'z': [True, False, True]})
 
-df2.head()
+df_.head()
 
 
 #%%
